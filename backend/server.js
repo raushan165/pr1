e703765ae8c9f9403b6cdf07da3a3190cae0cff1
Raushan Kumar <raushankumar261186@gmail.com>
2026-06-08 06:56:06 +0530

    new

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("./db");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Microservice running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
